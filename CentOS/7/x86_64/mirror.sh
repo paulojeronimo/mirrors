ce2908b850faafdcd796a121bec93faac7dbb0f1
Paulo Jerônimo <pj@paulojeronimo.info>
2014-07-28 05:08:58 -0300

    Added CentOS/7/x86_64 mirror Removed VM definitions from this project

---
+++ CentOS/7/x86_64/mirror.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Author: Paulo Jerônimo (@paulojeronimo, pj@paulojeronimo.info)
+#
+# This script clones a CentOS public mirror to use as a private
+# mirror in my teaching classes about Linux, JBoss and related
+# things.
+#
+# Some alternative rsync mirrors (near to Brazil)
+#
+#rsync://linorg.usp.br/CentOS/
+#rsync://mirrors.abdicar.com/costarica_CentOS/
+#rsync://centos.ufms.br/CentOS/
+
+CENTOS_VERSION=7
+RSYNC_DIR=${RSYNC_DIR:-rsync://linorg.usp.br/CentOS/$CENTOS_VERSION/}
+
+# CENTOS_MIRROR indicates the full PATH to the mirror directory. 
+# If you don't specify it, the mirror will be created in the same dir running this script.
+[ "$CENTOS_MIRROR" ] && \
+  d=$CENTOS_MIRROR || \
+  d=$(d=`dirname "$0"`/mirror; mkdir -p "$d" && cd "$d"; echo -n $PWD)
+
+[ -w "$d/" ] && unset sudo || sudo=sudo
+
+# This will rsync only CentOS x86_64 and exclude i386
+$sudo rsync --progress -avrt --delete --delete-excluded \
+--include "isos/x86_64/CentOS-7.0-1406-x86_64-NetInstall.iso" \
+--exclude "isos/x86_64/*" \
+--exclude "isos/i386" \
+--exclude "local*" \
+--exclude "centosplus" \
+--exclude "i386" \
+$RSYNC_DIR "$d/"
