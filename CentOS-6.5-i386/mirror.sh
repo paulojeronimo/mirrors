71b0d2cb54d1672fbd7e8cc774095f234010ce1d
Paulo Jerônimo <pj@paulojeronimo.info>
2014-11-10 23:27:43 -0200

    only_netinstall option added

--- CentOS-6.5-i386/mirror.sh
+++ CentOS-6.5-i386/mirror.sh
@@ -13,6 +13,7 @@
 
 CENTOS_VERSION=6.5
 RSYNC_DIR=${RSYNC_DIR:-rsync://linorg.usp.br/CentOS/$CENTOS_VERSION/}
+ONLY_NETINSTALL=${ONLY_NETINSTALL:-false}
 
 # CENTOS_MIRROR indicates the full PATH to the mirror directory. 
 # If you don't specify it, the mirror will be created in the same dir running this script.
@@ -22,12 +23,22 @@ RSYNC_DIR=${RSYNC_DIR:-rsync://linorg.usp.br/CentOS/$CENTOS_VERSION/}
 
 [ -w "$d/" ] && unset sudo || sudo=sudo
 
-# This will rsync only CentOS i386 and exclude x86_64
-$sudo rsync --progress -avrt --delete --delete-excluded \
---include "isos/i386/CentOS-$CENTOS_VERSION-i386-netinstall.iso" \
---exclude "isos/i386/*" \
---exclude "isos/x86_64" \
---exclude "local*" \
---exclude "centosplus" \
---exclude "x86_64" \
-$RSYNC_DIR "$d/"
+if $ONLY_NETINSTALL
+then
+    $sudo rsync --progress -avrt --delete --delete-excluded \
+    --include "isos/" \
+    --include "isos/i386/" \
+    --include "isos/i386/CentOS-$CENTOS_VERSION-i386-netinstall.iso" \
+    --exclude "*" \
+    $RSYNC_DIR "$d/"
+else
+    # This will rsync only CentOS i386 and exclude x86_64
+    $sudo rsync --progress -avrt --delete --delete-excluded \
+    --include "isos/i386/CentOS-$CENTOS_VERSION-i386-netinstall.iso" \
+    --exclude "isos/i386/*" \
+    --exclude "isos/x86_64" \
+    --exclude "local*" \
+    --exclude "centosplus" \
+    --exclude "x86_64" \
+    $RSYNC_DIR "$d/"
+fi
--- CentOS-6.5-x86_64/mirror.sh
+++ CentOS-6.5-x86_64/mirror.sh
@@ -13,6 +13,7 @@
 
 CENTOS_VERSION=6.5
 RSYNC_DIR=${RSYNC_DIR:-rsync://linorg.usp.br/CentOS/$CENTOS_VERSION/}
+ONLY_NETINSTALL=${ONLY_NETINSTALL:-false}
 
 # CENTOS_MIRROR indicates the full PATH to the mirror directory. 
 # If you don't specify it, the mirror will be created in the same dir running this script.
@@ -22,12 +23,22 @@ RSYNC_DIR=${RSYNC_DIR:-rsync://linorg.usp.br/CentOS/$CENTOS_VERSION/}
 
 [ -w "$d/" ] && unset sudo || sudo=sudo
 
-# This will rsync only CentOS x86_64 and exclude i386
-$sudo rsync --progress -avrt --delete --delete-excluded \
---include "isos/x86_64/CentOS-$CENTOS_VERSION-x86_64-netinstall.iso" \
---exclude "isos/x86_64/*" \
---exclude "isos/i386" \
---exclude "local*" \
---exclude "centosplus" \
---exclude "i386" \
-$RSYNC_DIR "$d/"
+if $ONLY_NETINSTALL
+then
+    $sudo rsync --progress -avrt --delete --delete-excluded \
+    --include "isos/" \
+    --include "isos/x86_64/" \
+    --include "isos/x86_64/CentOS-$CENTOS_VERSION-x86_64-netinstall.iso" \
+    --exclude "*" \
+    $RSYNC_DIR "$d/"
+else
+    # This will rsync only CentOS x86_64 and exclude i386
+    $sudo rsync --progress -avrt --delete --delete-excluded \
+    --include "isos/x86_64/CentOS-$CENTOS_VERSION-x86_64-netinstall.iso" \
+    --exclude "isos/x86_64/*" \
+    --exclude "isos/i386" \
+    --exclude "local*" \
+    --exclude "centosplus" \
+    --exclude "i386" \
+    $RSYNC_DIR "$d/"
+fi
